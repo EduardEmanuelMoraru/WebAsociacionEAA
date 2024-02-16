3e6cfdf8e97e224d205bb670f02a63ecc9925e70
Alvaro Almodovar <alvaro.almodovar1@educa.madrid.org>
2024-02-16 14:28:47 +0100

    plantilla hecha

---
+++ js/plantilla.js
@@ -0,0 +1,16 @@
+function rellenarDatos(){
+    var elemento =document.getElementById("login");
+    if(elemento.style.visibility=="hidden"){
+        elemento.style.visibility="visible";
+    }else{
+        elemento.style.visibility="hidden";
+    }
+}
+
+
+function irInicio() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+} //mostrar Y ocultar el div del login
