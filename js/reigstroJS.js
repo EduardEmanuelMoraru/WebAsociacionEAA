03b3c0804cc14c2efae760b3ed2318f858fc6a22
Alvaro Almodovar <alvaro.almodovar1@educa.madrid.org>
2024-02-16 23:29:17 +0100

    modificado las alturas y finalizado

--- js/reigstroJS.js
+++ js/reigstroJS.js
@@ -3,6 +3,7 @@ function confirmarDatos(){
  var nombre=document.getElementById("nombre").value;
  var apellidos=document.getElementById("apellido").value;
  var correo=document.getElementById("correo").value;
- var respuesta=confirm("¿Desea enviar esta información?"+"\n"+nombre+","+apellidos+","+correo);
+ var nie=document.getElementById("documento").value;
+ var respuesta=confirm("¿Desea enviar esta información?"+"\n"+nombre+","+apellidos+","+correo+","+nie);
  return respuesta;
 }
