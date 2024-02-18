92e5663929b453fe8a566a8fa63154b365b787b2
Eduard Moraru <eduard.moraru1@educa.madrid.org>
2024-02-18 14:51:04 +0100

    Arreglada mi estupidez

--- js/contacto.js
+++ js/contacto.js
@@ -1,11 +1,8 @@
-const $form = document.querySelector('#form')
-    const $buttonMailto = document.querySelector('#trucazo')
-
-    $form.addEventListener('submit', handleSubmit)
-
-    function handleSubmit(event) {
-      event.preventDefault()
-      const form = new FormData(this)
-      $buttonMailto.setAttribute('href', `mailto:eduard.emanue04@gmail.com?subject= ${form.get('name')} ${form.get('email')}&body=${form.get('message')}`)
-      $buttonMailto.click()
-    }
+function rellenarDatos(){
+  var elemento =document.getElementById("login");
+  if(elemento.style.visibility=="hidden"){
+      elemento.style.visibility="visible";
+  }else{
+      elemento.style.visibility="hidden";
+  }
+}
--- js/reigstroJS.js
+++
@@ -1,9 +0,0 @@
-
-function confirmarDatos(){
- var nombre=document.getElementById("nombre").value;
- var apellidos=document.getElementById("apellido").value;
- var correo=document.getElementById("correo").value;
- var nie=document.getElementById("documento").value;
- var respuesta=confirm("¿Desea enviar esta información?"+"\n"+nombre+","+apellidos+","+correo+","+nie);
- return respuesta;
-}
