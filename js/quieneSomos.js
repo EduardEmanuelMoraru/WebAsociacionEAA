6c2a3d6d604b02ea1428d1809cdf55eb5881b1bd
afl <alvaro.fernandez223@educa.madrid.org>
2024-02-19 11:49:20 +0100

    Retoque

---
+++ js/quieneSomos.js
@@ -0,0 +1,29 @@
+//mostrar Y ocultar el div del login
+function rellenarDatos(){
+    var elemento =document.getElementById("login");
+    if(elemento.style.visibility=="hidden"){
+        elemento.style.visibility="visible";
+    }else{
+        elemento.style.visibility="hidden";
+    }
+}
+
+function irInicio() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+function mostrarInformacion(id) {
+    var informacion = document.getElementById('info' + id.substr(-1));
+
+    // Oculta todas las informaciones
+    var todasLasInformaciones = document.querySelectorAll('.informacion');
+    todasLasInformaciones.forEach(function(info) {
+        info.style.display = 'none';
+    });
+
+    // Muestra la información correspondiente
+    informacion.style.display = 'block';
+}
